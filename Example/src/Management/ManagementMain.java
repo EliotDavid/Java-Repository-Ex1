1bdd22921517804b572bc66d083e7341b2afb85e
EliotDavid <optimistprimer@naver.com>
2022-10-19 13:22:36 +0900

    Example_1

--- Example/src/Management/ManagementMain.java
+++ Example/src/Management/ManagementMain.java
@@ -28,16 +28,42 @@ public class ManagementMain {
         //=========================================================
 
 
-
+        // ** 멤버 객체를 생성한다 
         Member member = new Member();
+        
+        //** 생성한 멤버 객체에 필드값을 셋팅한다
         member.setAge(32);
         member.setUserName("홍길동");
 
+        //** 멤버 객체와 has - a  관계인 Group class를 생성한다
+        Group group = new Group();
+        
+        //** 생성된 클래스의 필드값을 셋팅한다
+        group.set_class("K");
+        group.setSubject("JAVA");
 
+        //** 셋팅된 클래스를 멤버 클래스에 맵핑한다
+        member.setGroup(group);
+
+        //member.setGroup(new Group());
+
+
+        // ** 멤버의 필드값을 받아온다
         String name = member.getUserName();
         int age = member.getAge();
+        String _class = member.getGroup().get_class();
+        String subject = member.getGroup().getSubject();
 
-        System.out.println(name + " : " + age);
+        // ** 출력한다
+        System.out.println(name + " : " + age + " [" + _class + "]" + subject);
+
+
+
+        // ** 싱글톤은 단 하나만 만들어서 어디서든 접근가능하게 만든 클래스
+
+
+
+
 
 
 
--- Example/src/Management/Member.java
+++ Example/src/Management/Member.java
@@ -1,12 +1,30 @@
 package Management;
 
 public class Member extends Parent{
-// 이 관계는 is -a 관계임
+// 이렇게 상속 받은 관계는 is -a 관계임
 
     int Age;
     String userName;
 
+
+
     Group group; // 이 Mega가 소속이 될 수도 있고 회사가 될 수도 있음(어떤 그룹을 뜻함)
+    // ** 이 Member에서 group을 가졌다는 건 has - a 관계를 말함
+    // ** 이렇게 상속을 안 해줘도 내가 얼마든지 다른 클래스들을 가질 수 있음
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+       this.group = group;
+   }
+
+
+
+
+
+
 
 
 
