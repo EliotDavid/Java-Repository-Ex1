c7a70aad92071732b1363c244cc4d71ea6d8e73b
EliotDavid <optimistprimer@naver.com>
2022-11-15 14:11:23 +0900

    demo_8

---
+++ demo_8_1/src/main/java/com/example/demo_8_1/Demo81Application.java
@@ -0,0 +1,13 @@
+package com.example.demo_8_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo81Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo81Application.class, args);
+	}
+
+}
