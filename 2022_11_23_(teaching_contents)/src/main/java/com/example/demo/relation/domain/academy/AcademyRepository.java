910183afad7aa000b879a7e1fd3ebd677c475fca
EliotDavid <optimistprimer@naver.com>
2022-11-24 11:33:03 +0900

    2022_11_23(teaching_content)

---
+++ 2022_11_23_(teaching_contents)/src/main/java/com/example/demo/relation/domain/academy/AcademyRepository.java
@@ -0,0 +1,24 @@
+package com.example.demo.relation.domain.academy;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+
+@Repository
+public class AcademyRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Academy findById(Long id) {
+        return em.find(Academy.class, id);
+    }
+
+    public List<Academy> findAll() {
+        return em.createQuery("select a from Academy a", Academy.class).getResultList();
+    }
+
+}
---
+++ 2022_11_23_(teaching_contents)/src/main/java/com/example/demo/relation/domain/member/MemberRepository.java
@@ -0,0 +1,25 @@
+package com.example.demo.relation.domain.member;
+
+import com.example.demo.relation.domain.academy.Academy;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public List<Member> findByName(String academyName) {
+        return em.createQuery("select m from Member m join m.academy a where a.academyName = :name", Member.class)
+                .setParameter("name", academyName)
+                .getResultList();
+    }
+}
